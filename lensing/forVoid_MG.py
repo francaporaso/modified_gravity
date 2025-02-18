e7ec4e83f38d8b0b7fbd71050d96164ba46a9e29
Franco Caporaso <fmcaporaso@mi.unc.edu.ar>
2025-02-18 12:59:00 +0100

    un espacio...

--- lensing/forVoid_MG.py
+++ lensing/forVoid_MG.py
@@ -38,6 +38,7 @@ parser.add_argument('-N','--ndots', type=int, default=22, action='store')
 parser.add_argument('-K','--nk', type=int, default=100, action='store')    
 parser.add_argument('--addnoise', action='store_true')
 args = parser.parse_args()
+
 cosmo = LambdaCDM(H0=100.0*args.h_cosmo, Om0=args.Om0, Ode0=args.Ode0)
 
 #parameters
