5828cb7d4fd1a81f23fcc137a25c142ee0701914
Franco IATE <fmcaporaso@mi.unc.edu.ar>
2025-10-17 16:03:26 -0300

    update

--- statistics/vgcf.py
+++ statistics/vgcf.py
@@ -1,78 +1,15 @@
 from argparse import ArgumentParser
-from astropy.cosmology import Planck18 as cosmo
 from astropy.io import fits
-from astropy.table import Table
 import numpy as np
 import time
 import treecorr
 from multiprocessing import Pool
 
-import sys
-sys.path.append('/home/fcaporaso/modified_gravity/')
-from lensing.funcs import lenscat_load
-from stattools import make_randoms
+# import sys
+# sys.path.append('/home/fcaporaso/modified_gravity/')
+# from lensing.funcs import lenscat_load
+from stattools import Catalogs, make_randoms
 
-def comoving_distance(z):
-    return cosmo.comoving_distance(z).value # Mpc/h
-
-def ang2xyz(ra, dec, redshift):
-    comdist = comoving_distance(redshift)
-    x = comdist * np.cos(np.deg2rad(dec)) * np.cos(np.deg2rad(ra))
-    y = comdist * np.cos(np.deg2rad(dec)) * np.sin(np.deg2rad(ra))
-    z = comdist * np.sin(np.deg2rad(dec))
-    return x,y,z
-
-class Catalogs:
-    def __init__(self, lens_args, source_name, do_rands=True):
-        
-        ## [0]:rv, [1]:ra, [2]:dec, [3]:redshift
-        L, _, self.nvoids = lenscat_load(**lens_args)
-        assert self.nvoids != 0, 'No void found with those parameters!'
-        self.lenses = Table(L.T, names=['Rv', 'ra', 'dec', 'redshift'])
-        self.lenses['dcom'] = comoving_distance(self.lenses['redshift'])
-        
-        #self.sources = pd.read_parquet(source_name).sample(frac=1.0, random_state=1).to_numpy().T
-        try:
-            self.sources = Table.read(source_name, format='fits', memmap=True)
-        except FileNotFoundError:
-            self.sources = Table.read('/home/fcaporaso/cats/L768/'+source_name, format='fits', memmap=True)
-        mask = (self.sources['true_redshift_gal'] < lens_args["z_max"]+0.01) & (self.sources['true_redshift_gal'] >= lens_args["z_min"]-0.01)
-        self.sources = self.sources[mask]
-        self.ngals = len(self.sources)
-        assert self.ngals != 0, 'No tracer found with those parameters!'
-        
-        if 'dcom_gal' not in self.sources.columns:
-            self.sources['dcom_gal'] = comoving_distance(self.sources['true_redshift_gal'])
-
-        if do_rands:
-            print(' Making randoms '.center(60, '.'), flush=True)
-            self.random_lenses = Table(
-                make_randoms(
-                    self.lenses['ra'],
-                    self.lenses['dec'],
-                    self.lenses['dcom'],
-                    size_random=self.nvoids*10
-                ),
-                names=['ra','dec','dcom']
-            )
-
-            self.random_sources = Table(
-                make_randoms(
-                    self.sources['ra_gal'],
-                    self.sources['dec_gal'],
-                    self.sources['dcom_gal'], 
-                    size_random=self.ngals*10
-                ),
-                names=['ra_gal', 'dec_gal', 'dcom_gal']
-            )
-
-        else:
-            pass
-        
-        print('N voids: '.ljust(20,'.'), f' {self.nvoids:,}'.rjust(20,'.'),sep='',flush=True)
-        print('N sources: '.ljust(20,'.'), f' {self.ngals:,}'.rjust(20,'.'), sep='', flush=True)
-        print('N rand voids: '.ljust(20,'.'), f' {self.nvoids*10:,}'.rjust(20,'.'),sep='',flush=True)
-        print('N rand sources: '.ljust(20,'.'), f' {self.ngals*10:,}'.rjust(20,'.'),sep='',flush=True)
 
 class VoidGalaxyCrossCorrelation:
     
@@ -353,11 +290,15 @@ if __name__ == '__main__':
     simus = {
         'GR':{
             'lens':'voids_LCDM_09.dat',
-            'source':'l768_gr_z02-04_for01-02_19532.fits'
+            'source':'l768_gr_z02-04_for01-02_19532.fits',
+            'rand_lens':None,
+            'rand_source':None,
         },
         'fR':{
             'lens':'voids_fR_09.dat',
-            'source':'l768_mg_z02-04_for01-02_19531.fits'
+            'source':'l768_mg_z02-04_for01-02_19531.fits',
+            'rand_lens':None,
+            'rand_source':None,
         }
     }
 
@@ -366,11 +307,13 @@ if __name__ == '__main__':
         print(' '+f' EXECUTING -GR- ONLY '.center(60, '$')+' \n')
         lens_args['name']=simus['GR']['lens']
         main(sample=args.sample, tree_config=tree_config, lens_args=lens_args, source_name=simus['GR']['source'])
+
     elif args.fROnly:
         tin = time.time()
         print(' '+f' EXECUTING -f(R)- ONLY '.center(60, '$')+' \n')
         lens_args['name']=simus['fR']['lens']
         main(sample=args.sample, tree_config=tree_config, lens_args=lens_args, source_name=simus['fR']['source'])
+
     else:
         tin = time.time()
         for gravity in ['GR','fR']:
