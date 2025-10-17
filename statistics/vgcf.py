ff116684b2bacd5e98d6a170e662bf1577d608f6
Franco IATE <fmcaporaso@mi.unc.edu.ar>
2025-10-17 16:13:34 -0300

    update

--- statistics/vgcf.py
+++ statistics/vgcf.py
@@ -309,14 +309,14 @@ if __name__ == '__main__':
         print(' '+f' EXECUTING -GR- ONLY '.center(60, '$')+' \n')
         lens_args['name']=simus['GR']['lens']
         source_args['name']=simus['GR']['source']
-        main(sample=args.sample, tree_config=tree_config, lens_args=lens_args, source_args=simus['GR']['source'])
+        main(sample=args.sample, tree_config=tree_config, lens_args=lens_args, source_args=source_args)
 
     elif args.fROnly:
         tin = time.time()
         print(' '+f' EXECUTING -f(R)- ONLY '.center(60, '$')+' \n')
         lens_args['name']=simus['fR']['lens']
         source_args['name']=simus['fR']['source']
-        main(sample=args.sample, tree_config=tree_config, lens_args=lens_args, source_args=simus['fR']['source'])
+        main(sample=args.sample, tree_config=tree_config, lens_args=lens_args, source_args=source_args)
 
     else:
         tin = time.time()
@@ -324,7 +324,7 @@ if __name__ == '__main__':
             print(' '+f' EXECUTING -{gravity}- '.center(60, '$')+' \n')
             lens_args['name']=simus[gravity]['lens']
             source_args['name']=simus[gravity]['source']
-            main(sample=args.sample, tree_config=tree_config, lens_args=lens_args, source_args=simus[gravity]['source'])
+            main(sample=args.sample, tree_config=tree_config, lens_args=lens_args, source_args=source_args)
 
     print(f'Took {(time.time()-tin)/60.0} min'.center(50,':'), flush=True)
     print('End!', flush=True)
