e496e23febfad6ecf53fa4fcbe6ea5de797b0907
Franco IATE <fmcaporaso@mi.unc.edu.ar>
2025-11-04 10:07:08 -0300

    update

--- lensing/makeconfig.py
+++ lensing/makeconfig.py
@@ -29,49 +29,50 @@ print(f'{redshift=}')
 print(f'{delta=}')
 
 i = 0
-for zs in redshift:
-    for d in delta:
-        config = {
-            'NCORES':16,
-            'NK':100,
-            'BIN':'lin',
-            'prof':{
-                'NDOTS':20,
-                'RIN':0.01,
-                'ROUT':2.0
-            },
-            'void': {
-                'Rv_min':radii[0][0],
-                'Rv_max':radii[0][1],
-                'z_min':zs[0],
-                'z_max':zs[1],
-                'delta_min':d[0],
-                'delta_max':d[1]
-            },
-            'sim':{
-                'GR':{
-                    'lens':{
-                        'void08':'voids_LCDM_08.dat',
-                        'void09':'voids_LCDM_09.dat'
-                    },
-                    'source':{
-                        'for01-02':'l768_gr_z02-04_for01-02_w-pix64_19532.fits',
-                        'for02-03':'l768_gr_z04-07_for02-03_w-pix64_19304.fits'
-                    }
+for rv in radii:
+    for zs in redshift:
+        for d in delta:
+            config = {
+                'NCORES':16,
+                'NK':100,
+                'BIN':'lin',
+                'prof':{
+                    'NDOTS':20,
+                    'RIN':0.01,
+                    'ROUT':2.0
                 },
-                'fR':{
-                    'lens':{
-                        'void08':'voids_fR_08.dat',
-                        'void09':'voids_fR_09.dat',
-                    },
-                    'source':{
-                        'for01-02':'l768_mg_z02-04_for01-02_w-pix64_19531.fits',
-                        'for02-03':'l768_mg_z04-07_for02-03_w-pix64_19260.fits'
-                    }
+                'void': {
+                    'Rv_min':float(rv[0]),
+                    'Rv_max':float(rv[1]),
+                    'z_min':float(zs[0]),
+                    'z_max':float(zs[1]),
+                    'delta_min':float(d[0]),
+                    'delta_max':float(d[1])
                 },
+                'sim':{
+                    'GR':{
+                        'lens':{
+                            'void08':'voids_LCDM_08.dat',
+                            'void09':'voids_LCDM_09.dat'
+                        },
+                        'source':{
+                            'for01-02':'l768_gr_z02-04_for01-02_w-pix64_19532.fits',
+                            'for02-03':'l768_gr_z04-07_for02-03_w-pix64_19304.fits'
+                        }
+                    },
+                    'fR':{
+                        'lens':{
+                            'void08':'voids_fR_08.dat',
+                            'void09':'voids_fR_09.dat',
+                        },
+                        'source':{
+                            'for01-02':'l768_mg_z02-04_for01-02_w-pix64_19531.fits',
+                            'for02-03':'l768_mg_z04-07_for02-03_w-pix64_19260.fits'
+                        }
+                    },
 
-            },
-        }
-        with open(f'config_{i}.toml', 'w') as f:
-            toml.dump(config, f)
-        i+=1
+                },
+            }
+            with open(f'config_{i}.toml', 'w') as f:
+                toml.dump(config, f)
+            i+=1
