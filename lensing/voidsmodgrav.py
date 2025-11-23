c335d64fc00497f5fb97ce783d23878ac791024c
Franco Caporaso <fmcaporaso@mi.unc.edu.ar>
2025-11-23 01:03:59 -0300

    update

--- lensing/voidsmodgrav.py
+++ lensing/voidsmodgrav.py
@@ -6,6 +6,7 @@ from astropy.table import Table
 import healpy as hp
 from multiprocessing import Pool
 import numpy as np
+import os
 import time
 import toml
 from tqdm import tqdm
@@ -20,13 +21,18 @@ _N : int      = None
 _NK : int     = None
 _NCORES : int = None
 _S : Table    = None
+_PIX_TO_IDX : dict = {}
 _binspace = None
 _NSIDE : int = None
 
+REDSHIFT = "true_redshift_gal"
+
 def _init_globals(source_args, profile_args):
 
-    global _S, _NSIDE
-    global _RIN, _ROUT, _N, _NK, _NCORES, _binspace
+    global _S, _PIX_TO_IDX
+    global _NCORES, _RIN, _ROUT, _N, _NK, _NSIDE
+    global _binspace
+    global REDSHIFT
 
     _RIN    = profile_args['RIN']
     _ROUT   = profile_args['ROUT']
@@ -34,9 +40,35 @@ def _init_globals(source_args, profile_args):
     _NK     = profile_args['NK']
     _NCORES = profile_args['NCORES']
     _NSIDE = profile_args['NSIDE']
-    _binspace = {"lin": lambda s, e, n: np.linspace(s, e, n),
-                 "log": lambda s, e, n: np.logspace(np.log10(s), np.log10(e), n)}[profile_args['binning']]
+    _binspace = (np.linspace if profile_args['binning']=='lin' else
+                lambda start,end,n: np.logspace(np.log10(start), np.log10(end), n))
+    #_binspace = {"lin": lambda s, e, n: np.linspace(s, e, n),
+    #             "log": lambda s, e, n: np.logspace(np.log10(s), np.log10(e), n)}[profile_args['binning']]
+
     _S = sourcecat_load(**source_args)
+
+    if "true_redshift_gal" not in _S.columns:
+        REDSHIFT = "redshift_gal"
+
+    ## making a dict of healpix idx for fast query
+    upix, split_idx = np.unique(_S['pix'], return_index=True)
+    split_idx = np.append(split_idx, len(_S))
+    for i, pix in enumerate(upix):
+        _PIX_TO_IDX[int(pix)] = np.arange(split_idx[i], split_idx[i+1])
+
+def check_output_exists(output_file, overwrite=False):
+
+    if os.path.exists(output_file):
+        if not overwrite:
+            raise FileExistsError(
+                f'\n{"="*60}\n'
+                f'Output file already exists: {output_file}\n'
+                f'Use --overwrite flag to allow overwriting, or choose a different sample name.\n'
+                f'{"="*60}'
+            )
+        else:
+            print(f' WARNING: Will overwrite existing file: {output_file}', flush=True)
+    return True
 
 def sigma_crit(z_l, z_s):
     
@@ -49,14 +81,35 @@ def _get_masked_data(psi, ra0, dec0, z0):
     '''
     objects are selected by pixel on a disc of rad=psi+pad.
     pad = 0.1*psi
+    returns a copy of the data
     '''
 
-    mask_z = _S['true_redshift_gal']>z0+0.1
+    mask_z = _S[REDSHIFT]>z0+0.1
     pix_idx = hp.query_disc(_NSIDE, vec=hp.ang2vec(ra0, dec0, lonlat=True), radius=np.deg2rad(psi*1.1))
     mask = np.isin(_S['pix'], pix_idx, assume_unique=True)
     return _S[mask&mask_z]
 
-## TODO :: descargar el catalogo de nuevo... no tengo guardados los valores de redshift observado (ie con vel peculiares ie RSD)
+def _get_masked_idx_fast(psi, ra0, dec0, z0):
+    '''
+    objects are selected by pixel on a disc of rad=psi+pad where pad = 0.1*psi
+    uses prebuilt _PIX_TO_INDEX dict
+    returns the indices of _S where to select
+    '''
+
+    pix_idx = hp.query_disc(
+        _NSIDE, 
+        vec=hp.ang2vec(ra0, dec0, lonlat=True), 
+        radius=np.deg2rad(psi*1.1)
+    )
+    
+    idx_source = np.concatenate([
+        _PIX_TO_IDX[p] for p in pix_idx
+    ])
+
+    mask_z = _S[REDSHIFT][idx_source] > (z0+0.1)
+    
+    return idx_source[mask_z]
+
 def partial_profile(inp):
 
     Sigma_wsum    = np.zeros(_N)
@@ -71,19 +124,21 @@ def partial_profile(inp):
     DEGxMPC = cosmo.arcsec_per_kpc_proper(z0).to('deg/Mpc').value
     psi = DEGxMPC*_ROUT*Rv0
     
-    catdata = _get_masked_data(psi, ra0, dec0, z0)
-    #print(catdata.info, flush=True)
-
-    sigma_c = sigma_crit(z0, catdata['true_redshift_gal'])/Rv0
+    #catdata = _get_masked_data(psi, ra0, dec0, z0)
+    idx = _get_masked_idx_fast(psi, ra0, dec0, z0)
+    catdata = _S[idx]
+    
+    sigma_c = sigma_crit(z0, catdata[REDSHIFT])/Rv0
 
     rads, theta = eq2p2(
         np.deg2rad(catdata['ra_gal']), np.deg2rad(catdata['dec_gal']),
         np.deg2rad(ra0), np.deg2rad(dec0)
     )
 
-    ## TODO :: al descargar, cambiarle el signo
-    e1 = -catdata['gamma1']
-    e2 = -catdata['gamma2']
+    ## Si cosmohub=[19532,19531,19260,19304], cambiarle el signo
+    ## Si comoshub=[22833, 22834], el signo ya está cambiado
+    e1 = catdata['gamma1']
+    e2 = catdata['gamma2']
 
     #get tangential ellipticities 
     cos2t = np.cos(2.0*theta)
@@ -195,6 +250,8 @@ def execute_single_simu(config, args, gravity):
                    f'Rv{lens_args["Rv_min"]:02.0f}-{lens_args["Rv_max"]:02.0f}_'
                    f'z{100*lens_args["z_min"]:03.0f}-{100*lens_args["z_max"]:03.0f}_'
                    f'type{voidtype}_bin{profile_args["binning"]}.fits')
+    
+    check_output_exists(output_file, overwrite=args.overwrite)
 
     # === program arguments
     print(f' {" Settings ":=^60}')
@@ -220,45 +277,53 @@ def execute_single_simu(config, args, gravity):
 
     # ==== Calculating profiles
     Sigma, DSigma_t, DSigma_x, extradata = stacking(source_args, lens_args, profile_args)
-    cov_S   = cov_matrix(Sigma[1:,:])
-    cov_DSt = cov_matrix(DSigma_t[1:,:])
-    cov_DSx = cov_matrix(DSigma_x[1:,:])
     # =======================
 
     # ==== Saving
     head=fits.Header()
-    head['nvoids']=extradata['nvoids']
-    head['lenscat']=lens_args['name']
-    head['sourcat']=source_args['name']
-    head['Rv_min']=lens_args['Rv_min']
-    head['Rv_max']=lens_args['Rv_max']
-    head['Rv_mean']=extradata['Rv_mean']
-    head['z_min']=lens_args['z_min']
-    head['z_max']=lens_args['z_max']
-    head['z_mean']=extradata['z_mean']
-    head['voidtype']=voidtype
-    head['deltamin']=lens_args['delta_min']
-    head['deltamax']=lens_args['delta_max']
-    head['RIN']=profile_args['RIN']
-    head['ROUT']=profile_args['ROUT']
-    head['N']=profile_args['N']
-    head['NK']=profile_args['NK']
-    head['binning']=profile_args['binning']
-    head['HISTORY'] = f'{time.asctime()}'
+    head.update({
+        'nvoids':extradata['nvoids'],
+        'lenscat':lens_args['name'],
+        'sourcat':source_args['name'],
+        'Rv_min':lens_args['Rv_min'],
+        'Rv_max':lens_args['Rv_max'],
+        'Rv_mean':extradata['Rv_mean'],
+        'z_min':lens_args['z_min'],
+        'z_max':lens_args['z_max'],
+        'z_mean':extradata['z_mean'],
+        'voidtype':voidtype,
+        'deltamin':lens_args['delta_min'],
+        'deltamax':lens_args['delta_max'],
+        'RIN':profile_args['RIN'],
+        'ROUT':profile_args['ROUT'],
+        'N':profile_args['N'],
+        'NK':profile_args['NK'],
+        'binning':profile_args['binning'],
+        'HISTORY':f'{time.asctime()}',
+    })
 
-    table = Table({'Sigma':Sigma[0],'DSigma_t':DSigma_t[0],'DSigma_x':DSigma_x[0]})
+    table = Table({
+        'Sigma':Sigma[0],
+        'DSigma_t':DSigma_t[0],
+        'DSigma_x':DSigma_x[0]
+    })
+
     cov_hdu = [
-        fits.ImageHDU(cov_S, name='cov_Sigma'),
-        fits.ImageHDU(cov_DSt, name='cov_DSigma_t'),
-        fits.ImageHDU(cov_DSx, name='cov_DSigma_x'),
+        fits.ImageHDU(cov_matrix(Sigma[1:,:]), name='cov_Sigma'),
+        fits.ImageHDU(cov_matrix(DSigma_t[1:,:]), name='cov_DSigma_t'),
+        fits.ImageHDU(cov_matrix(DSigma_x[1:,:]), name='cov_DSigma_x'),
     ]
 
     primary_hdu = fits.PrimaryHDU(header=head)
     table_hdu = fits.BinTableHDU(table, name='profiles') 
  
-    hdul = fits.HDUList([primary_hdu, table_hdu] + cov_hdu)
-    hdul.writeto(output_file, overwrite=False)
+    hdul = fits.HDUList([
+        fits.PrimaryHDU(header=head), 
+        fits.BinTableHDU(table, name='profiles'),
+        *cov_hdu
+    ])
     
+    hdul.writeto(output_file, overwrite=args.overwrite)
     print(f' File saved in: {output_file}', flush=True)
 
 def main():
@@ -269,6 +334,7 @@ def main():
     parser.add_argument('--config', type=str, default='config.toml', action='store')
     parser.add_argument('--use08', action='store_true')
     parser.add_argument('--addnoise', action='store_true')
+    parser.add_argument('--overwrite', action='store_true')
     group = parser.add_mutually_exclusive_group()
     group.add_argument('--fROnly', action='store_true')
     group.add_argument('--GROnly', action='store_true')
