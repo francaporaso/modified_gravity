92cc7ed5085739d52ec7cd2f25dd4ae1d264c068
Franco Caporaso <fmcaporaso@mi.unc.edu.ar>
2026-08-20 13:08:01 -0300

    testing parquet

--- tests/catalogs.py
+++ tests/catalogs.py
@@ -1,7 +1,14 @@
+import numpy as np
 from lensing.io import read_sources_catalog
 
+print("reading parquet...")
 source = read_sources_catalog(
-    "/home/fcaporaso/cats/L768/l768_gr_z020-130_wpix64_23087.parquet", cat="parquet"
+    "/home/fcaporaso/cats/L768/l768_gr_z020-130_wpix64_23087.parquet",
+    cat="parquet",
+    use_threads=False,
 )
+print("done")
 
+print("check for pix order...")
 print(np.all(source["pix"][:-1] <= source["pix"][1:]))
+print("done")
